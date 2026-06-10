b09fd285841aba80067b0165529e268d3ec9e3f4
AlexHysel <alexhysel@gmail.com>
2026-06-10 16:12:24 +0800

    i dont remember what ive changed

--- MusicDatabase/obj/Debug/net10.0/MusicDatabase.AssemblyInfo.cs
+++ MusicDatabase/obj/Debug/net10.0/MusicDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913d658442aa01153be0a5f488073d4e78ccc8bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d39a7a148582677f55a7347e7b02ced975b3ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
