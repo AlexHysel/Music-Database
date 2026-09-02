6e64a218342c59c17818ad945ca44046e9dbfae7
AlexHysel <alexhysel@gmail.com>
2026-09-02 18:05:08 +0800

    add 'AddFavorite' endpoints

--- MusicDatabase/Controllers/UserControllers.cs
+++ MusicDatabase/Controllers/UserControllers.cs
@@ -35,6 +35,7 @@ public class UserController : ControllerBase
             return NotFound(result.Message);
     }
 
+    // FAVORITE ALBUMS
     [HttpGet("me/favorites/albums")]
     public async Task<IActionResult> GetFavoriteAlbums()
     {
@@ -51,6 +52,23 @@ public class UserController : ControllerBase
             return NotFound(result.Message);
     }
 
+    [HttpPost("me/favorites/albums")]
+    public async Task<IActionResult> AddFavoriteAlbum([FromQuery] Guid albumId)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? User.Identity?.Name;
+        if (userIdClaim == null) return Forbid();
+
+        if (!Guid.TryParse(userIdClaim, out var userId))
+            return BadRequest("Invalid user id in token");
+
+        Result result = await _orchestrator.AddAlbumToFavoritesAsync(albumId, userId);
+        if (result.Success)
+            return Ok();
+        else
+            return NotFound(result.Message);
+    }
+
+    // FAVORITE ARTISTS
     [HttpGet("me/favorites/artists")]
     public async Task<IActionResult> GetFavoriteArtists()
     {
@@ -63,6 +81,39 @@ public class UserController : ControllerBase
         Result<ArtistDTO[]> result = await _orchestrator.GetFavoriteArtistsAsync(userId);
         if (result.Success)
             return Ok(result.Data);
+        else
+            return NotFound(result.Message);
+    }
+
+    [HttpPost("me/favorites/artists")]
+    public async Task<IActionResult> AddFavoriteArtist([FromQuery] Guid artistId)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? User.Identity?.Name;
+        if (userIdClaim == null) return Forbid();
+
+        if (!Guid.TryParse(userIdClaim, out var userId))
+            return BadRequest("Invalid user id in token");
+
+        Result result = await _orchestrator.AddArtistToFavoritesAsync(artistId, userId);
+        if (result.Success)
+            return Ok();
+        else
+            return NotFound(result.Message);
+    }
+
+    // FAVORITE TRACKS
+    [HttpPost("me/favorites/tracks")]
+    public async Task<IActionResult> AddFavoriteTrack([FromQuery] Guid trackId)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? User.Identity?.Name;
+        if (userIdClaim == null) return Forbid();
+
+        if (!Guid.TryParse(userIdClaim, out var userId))
+            return BadRequest("Invalid user id in token");
+
+        Result result = await _orchestrator.AddTrackToFavoritesAsync(trackId, userId);
+        if (result.Success)
+            return Ok();
         else
             return NotFound(result.Message);
     }
